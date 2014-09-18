d3a8d63c7ee2a38d4467d23797ae5bfdb4deee0c
听着情歌流泪 <bobo.xiao@nmtree.com>
2014-09-18 16:08:45 +0800

    Github API

---
+++ github.js
@@ -0,0 +1,258 @@
+var Base64 = (function () {
+    var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+    var obj = {
+        /**
+         * Encodes a string in base64
+         * @param {String} input The string to encode in base64.
+         */
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3;
+            var enc1, enc2, enc3, enc4;
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) +
+                    keyStr.charAt(enc3) + keyStr.charAt(enc4);
+            } while (i < input.length);
+
+            return output;
+        },
+
+        /**
+         * Decodes a base64 string.
+         * @param {String} input The string to decode.
+         */
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3;
+            var enc1, enc2, enc3, enc4;
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            do {
+                enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+            } while (i < input.length);
+
+            return output;
+        }
+    };
+
+    return obj;
+})();
+if (typeof exports !== 'undefined') {
+    // Github = exports;
+    module.exports = Base64;
+} else {
+    window.Base64 = Base64;
+}
+
+(function() {
+
+  // Initial Setup
+  // -------------
+
+  var XMLHttpRequest, Base64, _;
+  if (typeof exports !== 'undefined') {
+      XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+      _ = require('underscore');
+      Base64 = require('./lib/base64.js');
+  }else{
+      _ = window._;
+      Base64 = window.Base64;
+  }
+  //prefer native XMLHttpRequest always
+  if (typeof window !== 'undefined' && typeof window.XMLHttpRequest !== 'undefined'){
+      XMLHttpRequest = window.XMLHttpRequest;
+  }
+
+
+  var API_URL = 'https://api.github.com';
+
+  var Github = function(options) {
+
+    // HTTP Request Abstraction
+    // =======
+    //
+    // I'm not proud of this and neither should you be if you were responsible for the XMLHttpRequest spec.
+
+    function _request(method, path, data, cb, raw, sync) {
+      function getURL() {
+        var url = path.indexOf('//') >= 0 ? path : API_URL + path;
+        return url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime();
+      }
+
+      var xhr = new XMLHttpRequest();
+      if (!raw) {xhr.dataType = "json";}
+
+      xhr.open(method, getURL(), !sync);
+      if (!sync) {
+        xhr.onreadystatechange = function () {
+          if (this.readyState == 4) {
+            if (this.status >= 200 && this.status < 300 || this.status === 304) {
+              cb(null, raw ? this.responseText : this.responseText ? JSON.parse(this.responseText) : true, this);
+            } else {
+              cb({path: path, request: this, error: this.status});
+            }
+          }
+        }
+      };
+      xhr.setRequestHeader('Accept','application/vnd.github.v3.raw+json');
+      xhr.setRequestHeader('Content-Type','application/json;charset=UTF-8');
+      if ((options.token) || (options.username && options.password)) {
+           xhr.setRequestHeader('Authorization', options.token
+             ? 'token '+ options.token
+             : 'Basic ' + Base64.encode(options.username + ':' + options.password)
+           );
+         }
+      data ? xhr.send(JSON.stringify(data)) : xhr.send();
+      if (sync) return xhr.response;
+    }
+
+    function _requestAllPages(path, cb) {
+      var results = [];
+      (function iterate() {
+        _request("GET", path, null, function(err, res, xhr) {
+          if (err) {
+            return cb(err);
+          }
+
+          results.push.apply(results, res);
+
+          var links = (xhr.getResponseHeader('link') || '').split(/\s*,\s*/g),
+              next = _.find(links, function(link) { return /rel="next"/.test(link); });
+
+          if (next) {
+            next = (/<(.*)>/.exec(next) || [])[1];
+          }
+
+          if (!next) {
+            cb(err, results);
+          } else {
+            path = next;
+            iterate();
+          }
+        });
+      })();
+    }
+
+
+
+    // User API
+    // =======
+
+    Github.User = function() {
+
+      this.show = function(username) {
+        var command = username ? "/users/"+username : "/user";
+        _request("GET", command, null, function(err, res) {
+          console.log(res);
+        });
+      };
+
+      this.follow = function(username) {
+        _request("PUT", "/user/following/"+username, null, function(err, res) {
+          console.log(res);
+        });
+      };
+
+      this.unfollow = function(username, cb) {
+        _request("DELETE", "/user/following/"+username, null, function(err, res) {
+          cb(err, res);
+        });
+      };
+
+    };
+
+    // Gist API
+
+    Github.Gist = function(options){
+      var id = options.id;
+      var gistPath = "/gists/"+id;
+      this.read = function() {
+        _request("GET", gistPath, null, function(err, gist) {
+          console.log(gist);
+        });
+      };
+    }
+    
+    // Repository API
+    // =======
+
+    Github.Repository = function(options) {
+      var repo = options.name;
+      var user = options.user;
+
+      var that = this;
+      var repoPath = "/repos/" + user + "/" + repo;
+
+      var currentTree = {
+        "branch": null,
+        "sha": null
+      };
+      this.show = function(cb) {
+        _request("GET", repoPath, null, cb);
+      };
+    }
+    // Top Level API
+    // -------
+
+    this.getUser = function() {
+      return new Github.User();
+    };
+    this.getGist = function(id) {
+      return new Github.Gist({id: id});
+    };
+    this.getRepo = function(user, repo) {
+      return new Github.Repository({user: user, name: repo});
+    };
+  };
+
+
+  if (typeof exports !== 'undefined') {
+    // Github = exports;
+    module.exports = Github;
+  } else {
+    window.Github = Github;
+  }
+
+}).call(this);
+
+var github = new Github({
+  token : 'af605e9303fabef95f13ddcddd6e830285383b6f'
+}); 
+
